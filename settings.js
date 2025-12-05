b9af076e2a1b101ea1e4f1f3c2b419e48a82c5c1
Snj <gamersnj577@gmail.com>
2025-12-05 14:53:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'fd48d06f46d36290' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ca0b888cdc2103bd' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
